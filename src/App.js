fb904424f8cd185624aec3d4536c3cae4a74d908
Ahimbisibwe Roland <rolandmbasa@gmail.com>
2019-01-03 22:58:04 +0300

    chore(setup) setup react router dom
    [Finishes #162967682]

--- src/App.js
+++ src/App.js
@@ -1,28 +1,16 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Provider } from 'react-redux';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import store from 'store';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <Provider store={store}>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact render={() => <h1>Hello world </h1>} />
+      </Switch>
+    </BrowserRouter>
+  </Provider>
+);
 
 export default App;
